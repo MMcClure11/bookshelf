d873af12dfeb571df3711f337444e1e39c61228d
MMcClure11 <mmcclure0100@gmail.com>
2024-01-16 09:39:33 -0600

    initial commit

---
+++ lib/bookshelf_web/components/layouts.ex
@@ -0,0 +1,5 @@
+defmodule BookshelfWeb.Layouts do
+  use BookshelfWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/bookshelf_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule BookshelfWeb.PageHTML do
+  use BookshelfWeb, :html
+
+  embed_templates "page_html/*"
+end
