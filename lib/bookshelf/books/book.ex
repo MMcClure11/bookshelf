416128a8e7d3c8ae18fe6092a62ecad57655e897
MMcClure11 <mmcclure0100@gmail.com>
2024-02-09 10:21:05 -0500

    Add Book struct

---
+++ lib/bookshelf/books/book.ex
@@ -0,0 +1,19 @@
+defmodule Bookshelf.Books.Book do
+  @moduledoc """
+  A struct for modeling a `Bookshelf.Books.Book`.
+  """
+
+  @type t() :: %__MODULE__{
+          title: String.t(),
+          author: String.t(),
+          genre: String.t(),
+          status: :want_to_read | :in_progress | :complete,
+          review: String.t(),
+          date_read: Date.t(),
+          cover_art: String.t()
+        }
+
+  @enforce_keys [:title, :author, :genre, :status]
+
+  defstruct [:title, :author, :genre, :review, :date_read, :status, :cover_art]
+end
