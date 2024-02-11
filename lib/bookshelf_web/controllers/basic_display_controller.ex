bb481663e0a48d06a472049410dfb0b49821d567
MMcClure11 <mmcclure0100@gmail.com>
2024-02-11 18:30:39 -0500

    Add basic_display index with table

--- lib/bookshelf_web/controllers/basic_display_controller.ex
+++ lib/bookshelf_web/controllers/basic_display_controller.ex
@@ -1,7 +1,10 @@
 defmodule BookshelfWeb.BasicDisplayController do
   use BookshelfWeb, :controller
 
+  alias Bookshelf.Books
+
   def index(conn, _params) do
+    conn = Plug.Conn.assign(conn, :books, Books.create_book_structs())
     render(conn, :index)
   end
 end
--- lib/bookshelf_web/controllers/basic_display_html.ex
+++ lib/bookshelf_web/controllers/basic_display_html.ex
@@ -3,7 +3,55 @@ defmodule BookshelfWeb.BasicDisplayHTML do
 
   def index(assigns) do
     ~H"""
-    The Bookshelf
+    <h1 class="text-xl font-bold text-slate-900 px-2 py-4">The Bookshelf</h1>
+    <table class="border-separate border-spacing-1 border border-slate-500 bg-slate-800">
+      <thead>
+        <tr>
+          <.column_header text="Title" />
+          <.column_header text="Author" />
+          <.column_header text="Genre" />
+          <.column_header text="Status" />
+          <.column_header text="Review" />
+          <.column_header text="Date Read" />
+        </tr>
+      </thead>
+      <tbody>
+        <%= for book <- @books do %>
+          <tr>
+            <.cell_data value={book.title} class="text-center" />
+            <.cell_data value={book.author} class="text-center" />
+            <.cell_data value={book.genre} class="text-center" />
+            <.cell_data value={parse_status(book.status)} class="text-center" />
+            <.cell_data value={book.review} class="whitespace-pre-line" />
+            <.cell_data value={book.date_read} class="text-center" />
+          </tr>
+        <% end %>
+      </tbody>
+    </table>
     """
   end
+
+  attr :text, :string, required: true
+
+  defp column_header(assigns) do
+    ~H"""
+    <th class="border border-slate-600 p-2 text-slate-100 bg-slate-500"><%= @text %></th>
+    """
+  end
+
+  attr :value, :string, required: true
+  attr :class, :string, default: ""
+
+  defp cell_data(assigns) do
+    ~H"""
+    <td class={["border border-slate-700 text-slate-200 bg-slate-600 p-2", @class]}>
+      <%= @value %>
+    </td>
+    """
+  end
+
+  @spec parse_status(:want_to_read | :in_progress | :complete) :: String.t()
+  defp parse_status(:want_to_read), do: "Want to read"
+  defp parse_status(:in_progress), do: "In progress…"
+  defp parse_status(:complete), do: "Complete"
 end
