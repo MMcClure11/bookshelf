605ec68bb4ff249ccd4259564be6d79598f517dc
MMcClure11 <mmcclure0100@gmail.com>
2024-03-05 16:02:09 -0600

    Add filtering for live-filter

--- lib/bookshelf_web/live/live_filter.ex
+++ lib/bookshelf_web/live/live_filter.ex
@@ -5,15 +5,50 @@ defmodule BookshelfWeb.LiveFilterLive do
 
   alias Bookshelf.Books
 
+  @impl Phoenix.LiveView
   def mount(_, _, socket) do
     {:ok, assign(socket, :books, Books.create_book_structs())}
   end
 
+  @impl Phoenix.LiveView
+  def handle_event("change", %{"search" => %{"query" => query}}, socket) do
+    {:noreply, assign(socket, :books, Books.filter_books(query))}
+  end
+
+  @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
     <h1 class="text-dragonhide-100 pb-6 font-['Kalnia'] text-[2rem] font-semibold leading-none">
       The Bookshelf
     </h1>
+
+    <form action="" novalidate="" role="search" phx-change="change" class="mb-16">
+      <div class="relative">
+        <input
+          id="search-input"
+          name="search[query]"
+          type="text"
+          class="bg-dragonhide-200 placeholder:text-dragonhide-400 text-dragonhide-600 focus:ring-dragonhide-300 h-12 w-80 rounded-sm border-none indent-10 text-base leading-none tracking-normal"
+          placeholder="Type to filter…"
+        />
+        <div class="bg-dragonhide-300 pointer-events-none absolute inset-y-0 flex w-11 items-center justify-center rounded-bl-sm rounded-tl-sm">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#594544"
+            class="h-5 w-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </div>
+      </div>
+    </form>
 
     <table class="w-full table-fixed text-left">
       <thead>
