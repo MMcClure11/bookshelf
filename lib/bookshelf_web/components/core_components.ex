3a768ef3287180b88884978a8471842ec53d7295
Meks (they/them) <mmcclure0100@gmail.com>
2024-03-18 14:57:40 -0600

    Add modal animation (#119)
    * Add modal animation
    
    * Make modal animation motion safe
    
    * Fix bug where filter query disappears when modal opened
    
    * Add book details struct
    
    * Refactor modal to always be present

--- lib/bookshelf_web/components/core_components.ex
+++ lib/bookshelf_web/components/core_components.ex
@@ -50,7 +50,11 @@ defmodule BookshelfWeb.CoreComponents do
       data-cancel={JS.exec(@on_cancel, "phx-remove")}
       class="relative z-50 hidden"
     >
-      <div id={"#{@id}-bg"} class="bg-zinc-50/90 fixed inset-0 transition-opacity" aria-hidden="true" />
+      <div
+        id={"#{@id}-bg"}
+        class="fixed inset-0 bg-black/[.66] transition-opacity"
+        aria-hidden="true"
+      />
       <div
         class="fixed inset-0 overflow-y-auto"
         aria-labelledby={"#{@id}-title"}
@@ -60,22 +64,22 @@ defmodule BookshelfWeb.CoreComponents do
         tabindex="0"
       >
         <div class="flex min-h-full items-center justify-center">
-          <div class="w-full max-w-3xl p-4 sm:p-6 lg:py-8">
+          <div class="w-full max-w-3xl">
             <.focus_wrap
               id={"#{@id}-container"}
               phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
               phx-key="escape"
               phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
-              class="shadow-zinc-700/10 ring-zinc-700/10 relative hidden rounded-2xl bg-white p-14 shadow-lg ring-1 transition"
+              class="bg-rust-900 relative hidden rounded-sm p-20 shadow-lg transition"
             >
-              <div class="absolute top-6 right-5">
+              <div class="absolute left-7 top-7 text-white">
                 <button
                   phx-click={JS.exec("data-cancel", to: "##{@id}")}
                   type="button"
-                  class="-m-3 flex-none p-3 opacity-20 hover:opacity-40"
+                  class="-m-3 flex-none p-3 opacity-80 hover:opacity-100"
                   aria-label={gettext("close")}
                 >
-                  <.icon name="hero-x-mark-solid" class="h-5 w-5" />
+                  <.icon name="hero-x-mark-solid" class="h-6 w-6" />
                 </button>
               </div>
               <div id={"#{@id}-content"}>
