9b963596d94257e219222976268d2abe2bee02ad
MMcClure11 <mmcclure0100@gmail.com>
2024-04-08 17:04:07 -0500

    Refactor to use icon function

--- lib/bookshelf_web/controllers/custom_styles_html.ex
+++ lib/bookshelf_web/controllers/custom_styles_html.ex
@@ -19,20 +19,7 @@ defmodule BookshelfWeb.CustomStylesHTML do
               class="bg-dragonhide-200 placeholder:text-dragonhide-400 text-dragonhide-600 h-12 w-80 rounded-sm indent-7 text-base leading-none tracking-normal"
             />
             <div class="pointer-events-none absolute inset-y-0 flex items-center pl-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="#594544"
-                class="h-5 w-5"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                />
-              </svg>
+              <.icon name="hero-magnifying-glass" class="h-5 w-5 bg-dragonhide-500" />
             </div>
           </div>
           <input
